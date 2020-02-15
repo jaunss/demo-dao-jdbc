6b6c7c611c9c4f7f0c6b0b857f144af445d1ba10
Joao Gabriel Maciel <joaogabriel36@hotmail.com>
2020-02-15 16:28:18 -0300

    Classe Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,18 @@
 package application;
 
+import java.util.Date;
+
 import entities.Department;
+import entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Department dp = new Department(1, "Livros");
+		Seller sl = new Seller(2, "Joao", "joaogcm_jb@outlook.com", new Date(), 23400.00, dp);
+		
 		System.out.println(dp);
+		System.out.println(sl);
 	}
 }
--- src/entities/Seller.java
+++ src/entities/Seller.java
@@ -1,8 +1,11 @@
 package entities;
 
+import java.io.Serializable;
 import java.util.Date;
 
-public class Seller {
+public class Seller implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
 	
 	private Integer id;
 	private String nome, email;
@@ -63,4 +66,37 @@ public class Seller {
 	public void setDepartamento(Department departamento) {
 		this.departamento = departamento;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Seller other = (Seller) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Seller [id = " + id + ", nome = " + nome + ", email = " + email + ", dataNascimento = " + dataNascimento
+				+ ", salarioBase = " + salarioBase + ", departamento = " + departamento + "]";
+	}
+	
+	
 }
